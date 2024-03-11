9262c79e703df19e3f9c4d66c5848a1e881dff44
Douglas6969 <133924667+Douglas6969@users.noreply.github.com>
2024-03-11 20:11:51 -0300

    3

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import { fastify } from "fastify";
 import { request } from "http";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { criarEnquete } from "./routes/criar-enquete";
+import { listarEnquete } from "./routes/listar-enquetes";
 
 const app = fastify()
 
@@ -11,50 +13,9 @@ const app = fastify()
     return 'Ola FASTIFY'
 })*/
 
-app.post('/criarEnquete', async (request, reply) => {
+app.register(criarEnquete)
 
-    const requestBody = z.object(
-        {
-            titulo: z.string(),
-            descricao: z.string(),
-            opcoesEnquete: z.array(z.string())
-        }
-    )
-    const { titulo, descricao, opcoesEnquete} = requestBody.parse(request.body);
-
-    const enqueteCriada = await prisma.enquete.create({
-        data : {
-            titulo,
-            descricao,
-            opcoesEnquete : {
-                createMany:{
-                    data : opcoesEnquete.map(opcao => {
-                        return {
-                            descricao : opcao
-                           
-                        }
-                    })
-                }
-            }
-
-        }
-    })
-
-  
-
-    return reply.status(201).send(enqueteCriada);   
-})
-
-app.get('/listarEnquete', async (request,reply) => {
-    const listaEnquete = await prisma.enquete
-    .findMany({
-        include:{
-            opcoesEnquete : true
-        }
-    })
-
-    return reply.send(listaEnquete)
-})
+app.register(listarEnquete)
 
 app.listen({port: 3333}).then( () => {
     console.log('SERVIDOR RODANDO')
